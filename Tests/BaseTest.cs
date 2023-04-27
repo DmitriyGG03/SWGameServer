84c3f46b22cf8bf2fae2ca47207593b328199f3e
Bogdan Avramencko <bogdan.avramencko425@gmail.com>
2023-04-27 20:20:20 +0300

    test

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Server;
+using SharedLibrary.Requests;
+using SharedLibrary.Responses;
+using System.Net.Http.Json;
+
+
+namespace Tests
+{
+	public class BaseTest
+	{
+		protected readonly HttpClient _httpClient;
+
+		protected const string _baseUrl = "https://localhost:7148";
+
+		public BaseTest()
+		{
+			var appFactory = new WebApplicationFactory<Program>().WithWebHostBuilder(host => {
+				host.ConfigureServices(services =>
+				{
+					var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<GameDbContext>));
+					services.Remove(descriptor);
+
+					services.AddDbContext<GameDbContext>(options => {
+						options.UseInMemoryDatabase("InMemoryDB");
+					});
+				});
+			});
+
+			_httpClient = appFactory.CreateClient();
+		}
+
+		public bool Register(string username, string email, string password)
+		{
+			var response = _httpClient.PostAsJsonAsync($"{_baseUrl}/authentication/register", new RegistrationRequest { Username = username, Email = email, Password = password }).Result;
+			if (response.IsSuccessStatusCode)
+			{
+				_httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", response.Content.ReadFromJsonAsync<AuthenticationResponse>().Result.Token);
+			}
+			return response.IsSuccessStatusCode;
+		}
+
+		public bool Login(string email, string password)
+		{
+			var response = _httpClient.PostAsJsonAsync($"{_baseUrl}/authentication/login", new LoginRequest { Email = email, Password = password }).Result;
+			if (response.IsSuccessStatusCode)
+			{
+				_httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", response.Content.ReadFromJsonAsync<AuthenticationResponse>().Result.Token);
+			}
+			return response.IsSuccessStatusCode;
+		}
+	}
+}
---
+++ Tests/LobbyControllerTest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SharedLibrary.Contracts.Hubs;
+using SharedLibrary.Models;
+using SharedLibrary.Requests;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace Tests
+{
+	public class LobbyControllerTest : BaseTest
+	{
+		const int port = 7148;
+		const string hubName = "lobby";
+		string accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUiLCJoZXJvIjoibnVsbCIsIm5iZiI6MTY4MjQyOTU4NywiZXhwIjoxOTk4MDQ4Nzg3LCJpYXQiOjE2ODI0Mjk1ODd9.LfSP4PpvU8uGIsxV5BqnZRRaByZBvGwFt6rhoRXTvFQ";
+
+		Lobby? currentLobby = null;
+		//HubConnection? connection;
+
+		public LobbyControllerTest()
+		{
+			Register("R1az", "test21@gmail.com", "ZfrwRgfs2");
+
+			//connection = new HubConnectionBuilder()
+			//	.WithUrl($"https://localhost:{port}/hubs/{hubName}", options => { options.AccessTokenProvider = () => Task.FromResult(accessToken); })
+			//	.Build();
+		}
+
+		[Fact]
+		public async Task CreateLobbyTest()
+		{
+			var response = await _httpClient.PostAsJsonAsync($"{_baseUrl}/Lobby", new CreateLobbyRequest { LobbyName = "Lobby1", MaxUsersCount = 4 });
+			Assert.True(response.IsSuccessStatusCode);
+		}
+
+		[Fact]
+		public async Task GetLobbiesTest()
+		{
+			var response = await _httpClient.GetAsync($"{_baseUrl}/Lobby");
+			Assert.True(response.IsSuccessStatusCode, response.StatusCode.ToString());
+		}
+
+		[Fact]
+		public async Task GetLobbyById()
+		{
+			var postLobbyResponse = await _httpClient.PostAsJsonAsync($"{_baseUrl}/Lobby", new CreateLobbyRequest { LobbyName = "Lobby1", MaxUsersCount = 4 });
+			var response = await _httpClient.GetAsync(postLobbyResponse.Headers.Location.ToString().Replace("Hero", "Lobby"));
+			Assert.True(response.IsSuccessStatusCode, response.StatusCode.ToString());
+		}
+
+		[Fact]
+		public async Task SignalRTest()
+		{
+			var connection = new HubConnectionBuilder()
+				.WithUrl($"https://localhost:{port}/hubs/{hubName}", options =>
+				{
+					options.AccessTokenProvider = () => Task.FromResult(accessToken);
+				})
+				.WithAutomaticReconnect()
+				.Build();
+
+			connection.On<Lobby>(ClientHandlers.Lobby.ConnectToLobbyHandler, (lobby) =>
+			{
+				currentLobby = lobby;
+			});
+
+			await connection.StartAsync();
+
+			var postLobbyResponse = await _httpClient.PostAsJsonAsync($"{_baseUrl}/Lobby", new CreateLobbyRequest { LobbyName = "Lobby1", MaxUsersCount = 4 });
+
+			string path = postLobbyResponse.Headers.Location.ToString();
+			int pos = path.LastIndexOf("/") + 1;
+			await connection.InvokeAsync(ServerHandlers.Lobby.ConnectToLobby, new Guid(path.Substring(pos, path.Length - pos)));
+
+			while (true)
+			{
+				if (currentLobby != null)
+				{
+					Assert.Equal("Lobby1", currentLobby.LobbyName);
+					break;
+				}
+			}
+		}
+	}
+}
