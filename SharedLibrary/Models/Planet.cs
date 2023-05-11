3201e6b1bd18820728a479ecb3f74a628a76d4b4
Vitaliy <bezuglivita123@gmail.com>
2023-05-11 13:38:17 +0300

    hero map view with dictionaries and collections

--- SharedLibrary/Models/Planet.cs
+++ SharedLibrary/Models/Planet.cs
@@ -10,13 +10,20 @@ namespace SharedLibrary.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
         public Guid Id { get; set; }
         public Point Position { get; set; }
+        [Range(0, Int32.MaxValue)]
+        public int Status { get; set; }
+        [Range(0, byte.MaxValue)]
+        public byte DaysNumber { get; set; }
         /* ctor for deserialization */
         public Planet()
-        { }
+        {
+            Status = (int)PlanetStatus.Known;
+        }
         public Planet(Point position)
         {
             Id = Guid.NewGuid();
             Position = position;
+            Status = (int) PlanetStatus.Researched;
         }
     }
 }
