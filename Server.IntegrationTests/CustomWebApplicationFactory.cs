f94bc01c0a5efd8ef657608a7d4f14ff18200914
Bohdan Avramencko <68287603+g0odf3els@users.noreply.github.com>
2023-05-27 22:17:09 +0300

    changed-tests-and-added-next-turn-test

---
+++ Server.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server.IntegrationTests
+{
+	public class CustomWebApplicationFactory<TProgram>: WebApplicationFactory<TProgram> where TProgram : class
+	{
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			builder.ConfigureServices(services =>
+			{
+				var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<GameDbContext>));
+				services.Remove(dbContextDescriptor);
+
+				var dbConnectionDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(GameDbContext));
+				services.Remove(dbConnectionDescriptor);
+
+				var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<GameDbContext>));
+				services.Remove(descriptor);
+
+				services.AddDbContext<GameDbContext>(options => {
+					options.UseInMemoryDatabase("InMemoryDB");
+				});
+			});
+
+			builder.UseEnvironment("Development");
+		}
+	}
+}
